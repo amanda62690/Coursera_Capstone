8bb1bc9519fd23485633e1fe41ca51d02a69d795
amanda62690 <81632346+amanda62690@users.noreply.github.com>
2021-08-05 08:11:08 -0400

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# This notebook will be used for the Applied Data Science capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
